1792e2949b42f67777b464659a20020669bfaaed
Slowvik <72633088+Slowvik@users.noreply.github.com>
2024-03-12 21:29:49 +0530

    Update testcases.cpp

--- src/Problem 5/testcases.cpp
+++ src/Problem 5/testcases.cpp
@@ -159,16 +159,5 @@ int main()
     endTime = std::chrono::high_resolution_clock::now();
     std::cout<<std::setw(20) << std::left <<test_size<<std::setw(40) << std::left<< "50:50 - AuctionPrices with flat_hash_map:"<<std::setw(20) << std::right <<std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(endTime - startTime).count()<<" ms"<<std::endl;
 
-
-
-
-    /*
-    Conclusions:
-    1. hashing before inserting string,int takes MUCH longer (30% longer with 100,000 insertions, 100% longer with 500,000 insertions, 160% longer with 1,000,000 insertions). This is because we are actually performing two inserts: one in hashed string vs string and the other in the regular orderbook. After checking the performance, unordered_map works better than map for the hasher_lookup data structure.
-
-    2. the reverse is true for deletions: deleting from hashed map is MUCH faster (about 400% faster with 1,000,000 entries)
-    */
-
-
     return 0;
 }
